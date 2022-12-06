80f08c0ae1ee8d41296853321434cabb6647cbec
Klaus Schönhofen <surfnik@gmail.com>
2022-12-06 17:43:36 +0100

    _07 Listen binden erstellt inclusive 08 CustomLayout List + 09 Selektiertes Element

---
+++ _07 Listen binden/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Listen_binden.Models;
+[ObservableObject]
+public partial class Person
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Fullname))]
+    private string firstname;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Fullname))]
+    private string lastname;
+    [ObservableProperty]
+    private string department;
+
+    public string Fullname => $"{Firstname} {Lastname}";
+
+}
---
+++ _07 Listen binden/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using _07_Listen_binden.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Listen_binden.ViewModels
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty]
+        private Person newPerson = new Person();
+        [ObservableProperty]
+        private ObservableCollection<Person> persons = new ObservableCollection<Person>();
+
+        public MainWindowViewModel()
+        {
+            AddPersonCommand = new RelayCommand<Person>((o) =>
+            {
+                Persons.Add(NewPerson);
+                NewPerson = new Person();
+            });
+        }
+
+        public RelayCommand<Person> AddPersonCommand { get; }
+
+    }
+}
