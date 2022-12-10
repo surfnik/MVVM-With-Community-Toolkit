3241d9d96a5e29622aa03763c451c8b480c34498
Klaus Schönhofen <surfnik@gmail.com>
2022-12-10 17:33:08 +0100

    _13 Animationen und Styles Trigger erstellt und mit Dummy-Daten gefüllt

---
+++ _13 Animationen und Styles Trigger/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Animationen_und_Styles_Trigger.ViewModels;
+[ObservableObject]
+public partial class Person
+{
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Fullname))]
+    private string firstname;
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Fullname))]
+    private string lastname;
+    [ObservableProperty]
+    private string department;
+
+    public Person(string firstname, string lastname, string department)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Department = department;
+    }
+
+    public Person()
+    {
+    }
+
+    public string Fullname => $"{Firstname} {Lastname}";
+
+}
---
+++ _13 Animationen und Styles Trigger/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Animationen_und_Styles_Trigger.ViewModels;
+[ObservableObject]
+public partial class MainWindowViewModel
+{
+    [ObservableProperty]
+    private Person newPerson = new Person();
+    [ObservableProperty]
+    private ObservableCollection<Person> persons = new ObservableCollection<Person>();
+
+    public MainWindowViewModel()
+    {   // Drei Personen erstellt, damit man sie nicht immer wieder neu eingeben muss
+        Persons.Add(new Person("Susi", "Sonnenschein", "Sales"));
+        Persons.Add(new Person("Hans", "Müller", "Development"));
+        Persons.Add(new Person("Hugo", "Bossinger", "Management"));
+    }
+    [RelayCommand]
+    private void AddPerson()
+    {
+        Persons.Add(NewPerson);
+        NewPerson = new Person();
+    }
+}
