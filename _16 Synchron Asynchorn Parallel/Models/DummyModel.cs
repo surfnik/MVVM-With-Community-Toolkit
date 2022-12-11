123ec04b5bf74ddc535cf6fc9b8dc065e3530722
Klaus Schönhofen <surfnik@gmail.com>
2022-12-11 18:07:02 +0100

    _16 Synchron Asynchorn Parallel erstellt

---
+++ _16 Synchron Asynchorn Parallel/Models/DummyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Synchron_Asynchorn_Parallel.Models
+{
+    [ObservableObject]
+    public partial class DummyModel
+    {
+        [ObservableProperty]
+        private string? prop1;
+    }
+}
---
+++ _16 Synchron Asynchorn Parallel/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Synchron_Asynchorn_Parallel.ViewModels
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [ObservableProperty]
+        public string dummyText;
+
+        public MainWindowViewModel()
+        {
+            DummyText = "Ein Platzhalter";
+        }
+
+    }
+}
