875f6ae2f55da8e05706db6f2df812c2c137c47a
Klaus Schönhofen <surfnik@gmail.com>
2023-01-07 15:38:27 +0100

    24 UserControls  erstellt

---
+++ _24 UserControls/Components/MaxLengthTextBoxUserControll.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace _24_UserControls.Components
+{
+    /// <summary>
+    /// Interaktionslogik für MaxLengthTextBoxUserControll.xaml
+    /// </summary>
+    public partial class MaxLengthTextBoxUserControll : UserControl
+    {
+        public MaxLengthTextBoxUserControll()
+        {
+            InitializeComponent();
+            this.DataContext= this;
+        }
+
+        public string Caption { get; set; }
+        public int MaxLength { get; set; }
+
+    }
+}
