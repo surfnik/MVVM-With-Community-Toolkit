94f3b5c526c30a5b5f23cda7d37795234457ac25
Klaus Schönhofen <surfnik@gmail.com>
2023-01-16 17:05:42 +0100

    Reset

---
+++ _25 UserControls Binden Dependency Properties/Models/DummyModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25_UserControls_Binden_Dependency_Properties.Models
+{
+    [ObservableObject]
+    public partial class DummyModel
+    {
+        [ObservableProperty]
+        private string? prop1;
+    }
+}
